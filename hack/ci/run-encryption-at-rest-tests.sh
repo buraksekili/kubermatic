2c72cd57f6b86611d2bd0fd507f2f1192f12e5f7
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2025-04-09 17:05:08 +0300

    fix timeout
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- hack/ci/run-encryption-at-rest-tests.sh
+++ hack/ci/run-encryption-at-rest-tests.sh
@@ -52,7 +52,7 @@ source hack/ci/setup-kubermatic-backups-in-kind.sh
 
 echodate "Running encryption-at-rest tests..."
 
-go_test encryption_at_rest -timeout 120m -tags e2e,ee -v ./pkg/test/e2e/encryption-at-rest \
+go_test encryption_at_rest -timeout 60m -tags e2e,ee -v ./pkg/test/e2e/encryption-at-rest \
   -byo-kkp-datacenter byo-kubernetes \
   -ssh-pub-key "$(cat "$E2E_SSH_PUBKEY")"
 
